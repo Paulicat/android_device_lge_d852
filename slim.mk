10d25d7262875dd783432b20d51774dfaf0e7fac
Paulicat <antonpaulic@gmail.com>
2017-01-05 10:16:49 -0500

    slim6 bringup

---
+++ slim.mk
@@ -0,0 +1,10 @@
+$(call inherit-product, device/lge/d852/full_d852.mk)
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+
+# Boot animation
+TARGET_SCREEN_WIDTH := 1440
+TARGET_SCREEN_HEIGHT := 2560
+
+PRODUCT_RELEASE_NAME := LG G3
+PRODUCT_NAME := slim_d852
